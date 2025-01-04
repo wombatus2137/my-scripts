f4b00027d6cfdb8dff68337c62b315b0349f852f
wombatus2137 <125801970+wombatus2137@users.noreply.github.com>
2025-01-04 15:58:01 +0100

    Add GH extension update

--- Update_everything.ps1
+++ Update_everything.ps1
@@ -4,3 +4,4 @@ Updates all programs and PowerShell modules installed on a sysytem
 #>
 winget upgrade -r -u --accept-package-agreements --accept-source-agreements
 Update-Module
+gh extension upgrade --all
