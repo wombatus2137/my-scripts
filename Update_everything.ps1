643f167d0773f06fe697f452dfaff84fe5e25e5c
wombatus2137 <125801970+wombatus2137@users.noreply.github.com>
2025-04-16 21:32:15 +0200

    Add pip update

--- Update_everything.ps1
+++ Update_everything.ps1
@@ -5,3 +5,4 @@ Updates all programs and PowerShell modules installed on a sysytem
 winget upgrade -r -u --accept-package-agreements --accept-source-agreements
 Update-Module
 gh extension upgrade --all
+python.exe -m pip install --upgrade pip
