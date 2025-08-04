418c3550f99fd94af1aa334ae7b8243312d52b12
wombatus2137 <125801970+wombatus2137@users.noreply.github.com>
2025-08-04 21:07:10 +0200

    fix(Update_everything.ps1): Don't update unknown version packages

--- Update_everything.ps1
+++ Update_everything.ps1
@@ -2,7 +2,7 @@
 .SYNOPSIS
 Updates all programs and PowerShell modules installed on a sysytem
 #>
-winget upgrade -r -u --accept-package-agreements --accept-source-agreements
+winget upgrade -r --accept-package-agreements --accept-source-agreements
 Update-Module
 gh extension upgrade --all
 python.exe -m pip install --upgrade pip
