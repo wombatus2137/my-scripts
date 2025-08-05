ee02cdaca260d5134f5393a0ad847675d088cd06
wombatus2137 <125801970+wombatus2137@users.noreply.github.com>
2025-08-05 20:21:59 +0200

    fix(Config_Git.ps1): Move Git config into bash script to fix some bugs

--- Config_Git.ps1
+++ Config_Git.ps1
@@ -3,11 +3,14 @@
 .SYNOPSIS
 Configures Git with GPG signing
 #>
-param ( $Email, $UserName )
+param ( $Email, $UserName, $KeyID )
 $EmailTest = $null -eq $Email
 $UserNameTest = $null -eq $UserName
 if ( $EmailTest -or $UserNameTest ) {
     throw 'No email or username provided, input them as an argument'
+}
+if ( $null -eq $KeyID ) {
+    $KeyID = '$KeyID'
 }
 function RefreshPath {
     $env:Path = [System.Environment]::GetEnvironmentVariable( 'Path', 'Machine' ) + ';' + [System.Environment]::GetEnvironmentVariable( 'Path', 'User' )
@@ -18,16 +21,15 @@ winget install --id Git.Git -e --source winget -i
 RefreshPath
 
 #!GPG provided by Git is partially broken, so this is a workaround
-#Export script to bash to prepare GPG key
-$BashPart = @'
-gpg --full-generate-key
-gpg --list-secret-keys --keyid-format=long
-read -p 'Enter enter key ID you want to use: ' KeyID
+#Export script to bash
+$BashPart = @"
+#Prepare GPG key
+if [ -z "$KeyID" ]; then
+    gpg --full-generate-key
+    gpg --list-secret-keys --keyid-format=long
+    read -p 'Enter enter key ID you want to use: ' KeyID
+fi
 gpg --armor --export $KeyID
-'@
-Write-Output $BashPart | Out-File BashPart.sh
-& $env:ProgramFiles'\Git\usr\bin\bash.exe' -l BashPart.sh
-Remove-Item -Path BashPart.sh
 
 #Configure Git
 git config --global user.email "$Email"
@@ -36,6 +38,10 @@ git config --global --unset gpg.format
 git config --global user.signingkey $KeyID
 git config --global commit.gpgsign true
 git config --global tag.gpgSign true
+"@
+Write-Output $BashPart | Out-File BashPart.sh
+& $env:ProgramFiles'\Git\usr\bin\bash.exe' -l BashPart.sh
+Remove-Item -Path BashPart.sh
 
 #Configure GH CLI
 winget install --id GitHub.cli
