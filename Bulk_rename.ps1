d48e600135a8fa2cc4e115bf80aae18dbacebdd9
wombatus2137 <125801970+wombatus2137@users.noreply.github.com>
2025-10-25 17:10:19 +0200

    fix(bulk_rename.ps1): change UsedDates to correct scope

--- Bulk_rename.ps1
+++ Bulk_rename.ps1
@@ -11,12 +11,17 @@ function CheckName {
         $i++
         $ProposedName = $OriginalProposedName + ' (' + $i + ")"
     }
+    $script:UsedDates += "$ProposedName"
     return $ProposedName
 }
 
-Get-ChildItem | ForEach-Object -Process {
-    $CreationDate = $_.CreationTime.ToString( ( Get-Culture ) )
+Get-ChildItem -File | Sort-Object -Property CreationTime | ForEach-Object -Process {
+    if ( $_.LastWriteTime -le $_.CreationTime ) {
+        $CreationDate = $_.LastWriteTime.ToString( 'dd-MM-yyyy' )
+    } else {
+        $CreationDate = $_.CreationTime.ToString( 'dd-MM-yyyy' )
+    }
     $Extension = $_.Extension
     $FinalName = CheckName -ProposedName $CreationDate
-    Rename-Item -Path $_.Name -NewName $FinalName$Extension
+    Rename-Item -Path $_.Name -NewName "$FinalName$Extension"
 }
