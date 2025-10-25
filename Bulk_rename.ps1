51fbe291ed9740379c4582a1c2cdbdb1f2430c2c
wombatus2137 <125801970+wombatus2137@users.noreply.github.com>
2025-10-25 13:01:39 +0200

    feat(bulk_rename.ps1): ensure file name uniqueness

--- Bulk_rename.ps1
+++ Bulk_rename.ps1
@@ -2,8 +2,21 @@
 .SYNOPSIS
 Renames all files in current directory to their creation dates
 #>
+$UsedDates = @()
+function CheckName {
+    param ( $ProposedName )
+    $i = 0
+    $OriginalProposedName = $ProposedName
+    while ( $UsedDates -contains $ProposedName) {
+        $i++
+        $ProposedName = $OriginalProposedName + ' (' + $i + ")"
+    }
+    return $ProposedName
+}
+
 Get-ChildItem | ForEach-Object -Process {
     $CreationDate = $_.CreationTime.ToString( ( Get-Culture ) )
     $Extension = $_.Extension
-    Rename-Item -Path $_.Name -NewName $CreationDate$Extension
+    $FinalName = CheckName -ProposedName $CreationDate
+    Rename-Item -Path $_.Name -NewName $FinalName$Extension
 }
